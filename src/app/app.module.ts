aaf6c948745301feca13c7a1fbf7a56ea94491a5
khxxhy <135722697+khxxhy@users.noreply.github.com>
2024-08-29 16:32:16 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,46 @@ import { AngularFireAuthModule } from '@angular/fire/auth'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
-import { NavbarComponent } from './groo-store/navbar/navbar.component';
+import { Router, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { LoginComponent } from './login/login.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    ShoppingCartComponent,
+    AdminProductsComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'check-out', component: CheckOutComponent},
+      {path: 'shopping-cart', component: ShoppingCartComponent},
+      {path: '', component: OrderSuccessComponent},
+      {path: 'login', component: LoginComponent},
+
+      {path: 'admin-products', component: AdminProductsComponent},
+      {path: 'admin-orders', component: AdminOrdersComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
